6e1eaf7cda2137330bc5e8cffbbf0907f7e755d9
Edinaildo <edinaildopereira2013@gmail.com>
2022-09-15 14:43:47 -0300

    first commit

---
+++ src/tasksapi.js
@@ -0,0 +1,55 @@
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:3000'
+
+export default {
+  // Retorna lista de tarefas
+  getTasks: (cb) => {
+    axios
+      .get('/tasks')
+      .then((response) => {
+        cb(response.data)
+      })
+      .catch((error) => {
+        console.log('error:', error)
+      })
+  },
+  // Salva nova tarefa
+  postTask: (task) => {
+    return new Promise((resolve, reject) => {
+      axios
+        .post('/tasks', task)
+        .then((response) => {
+          return resolve(response.data)
+        })
+        .catch((error) => {
+          return reject(error)
+        })
+    })
+  },
+  // Deleta tarefa
+  deleteTasks: (taskId) => {
+    return new Promise((resolve, reject) => {
+      axios
+        .delete(`/tasks/${taskId}`)
+        .then((response) => {
+          return resolve(response.data)
+        })
+        .catch((error) => {
+          return reject(error)
+        })
+    })
+  },
+  // Editar tarefa
+  editTask: (taskid, task) => {
+    return new Promise((resolve, reject) => {
+      axios
+        .put(`/tasks/${taskid}`, task)
+        .then((response) => {
+          return resolve(response.data)
+        })
+        .catch((error) => {
+          return reject(error)
+        })
+    })
+  },
+}
---
+++ src/views/NewTaskView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-text-field v-model="newTask.title" label="Title"></v-text-field>
+    <v-text-field v-model="newTask.project" label="Project"></v-text-field>
+    <v-text-field v-model="newTask.dueTo" label="Dueto"></v-text-field>
+    <v-btn x-large rounded color="primary" dark @click="createTask">
+      Criar
+    </v-btn>
+    <!-- <v-btn x-large rounded color="primary" dark :to="{ name: 'taskList' }">
+      Listar tarefas
+    </v-btn> -->
+  </div>
+</template>
+
+<script>
+import TasksApi from '@/tasksapi.js'
+
+export default {
+  data: () => {
+    return {
+      newTask: {
+        title: '',
+        project: '',
+        dueTo: '',
+      },
+    }
+  },
+  methods: {
+    createTask() {
+      TasksApi.postTask(this.newTask)
+      this.$router.push({ name: 'taskList' })
+    },
+  },
+}
+</script>
